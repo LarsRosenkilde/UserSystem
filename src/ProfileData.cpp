7d07f0aa3fe4b2f1053a2670c2e4b3c0ed9b94f9
Lars Rosenkilde <lars.rosenkilde95@gmail.com>
2022-01-25 12:18:36 +0100

    ProfileData Downscale

--- src/ProfileData.cpp
+++ src/ProfileData.cpp
@@ -14,80 +14,6 @@ void ProfileData::setEmail(string email) {
     Email = move(email);
 }
 
-void ProfileData::setFirstname(string firstname) {
-    if (firstname[0] > 96 && firstname[0] < 126) {
-        firstname[0] = char(firstname[0] - 32);
-    }
-    Firstname = move(firstname);
-}
-
-void ProfileData::setLastname(string lastname) {
-    if (lastname[0] > 96 && lastname[0] < 126) {
-        lastname[0] = char(lastname[0] - 32);
-    }
-    Lastname = move(lastname);
-}
-
-void ProfileData::setGender(string gender) {
-    Gender = move(gender);
-}
-
-void ProfileData::setAge(signed int age) {
-    Age = age;
-}
-
-void ProfileData::setHeight(double height) {
-    Height = height;
-}
-
-void ProfileData::setWeight(double weight) {
-    Weight = weight;
-}
-
-void ProfileData::setBmi(double bmi) {
-    Bmi = bmi;
-}
-
-//string ProfileData::getUsername() {
-//    return Username;
-//}
-//
-//string ProfileData::getPassword() {
-//    return Password;
-//}
-//
-//string ProfileData::getEmail() {
-//    return Email;
-//}
-//
-//string ProfileData::getFirstname() {
-//    return Firstname;
-//}
-//
-//string ProfileData::getLastname() {
-//    return Lastname;
-//}
-//
-//string ProfileData::getGender() {
-//    return Gender;
-//}
-//
-//int ProfileData::getAge() const {
-//    return Age;
-//}
-//
-//double ProfileData::getHeight() {
-//    return Height;
-//}
-//
-//double ProfileData::getWeight() {
-//    return Weight;
-//}
-
-double ProfileData::getBmi() const {
-    return Bmi;
-}
-
 bool ProfileData::validateUniqueUsername(const string& Username) {
     string users;
     string username;
@@ -148,48 +74,6 @@ bool ProfileData::validatePassword(const string& Password) {
     return false;
 }
 
-bool ProfileData::validateFirstname(const string& FirstName) {
-    if (!regex_match(FirstName, regex(".*[0-9].*"))) {
-        return true;
-    }
-    return false;
-}
-
-bool ProfileData::validateLastname(const string& LastName) {
-    if (!regex_match(LastName, regex(".*[0-9].*"))) {
-        return true;
-    }
-    return false;
-}
-
-bool ProfileData::validateGender(const string& Gender) {
-    if (Gender == "male" || Gender == "female") {
-        return true;
-    }
-    return false;
-}
-
-bool ProfileData::validateAge(const int& Age) {
-    if (Age > 0 && Age < 150) {
-        return true;
-    }
-    return false;
-}
-
-bool ProfileData::validateHeight(const double& Height) {
-    if (Height > 0 && Height < 3) {
-        return true;
-    }
-    return false;
-}
-
-bool ProfileData::validateWeight(const double& Weight) {
-    if (Weight > 0 && Weight < 300) {
-        return true;
-    }
-    return false;
-}
-
 bool ProfileData::loginUser() {
     string users;
     string username;
@@ -221,28 +105,11 @@ void ProfileData::signupUser() {
     usersList.open("temp.csv", ios_base::app);
     usersList << Username << ',';
     usersList << Password << ',';
-    usersList << Email << ',';
-    usersList << Firstname << ',';
-    usersList << Lastname << ',';
-    usersList << Gender << ',';
-    usersList << Age << ',';
-    usersList << Height << ',';
-    usersList << Weight << ',';
-    usersList << Bmi << "\n";
+    usersList << Email << '\n';
 
     usersList.close();
     cout << "User saved successfully!\n" << endl;
     crypt.encrypt();
-}
-
-double ProfileData::roundNumber(double number) {
-    double round_off = pow(10.0f, 2.0);
-    return round(number * round_off) / round_off;
-}
-
-void ProfileData::calcBmi() {
-    double NewBmi = Weight / (pow(Height, 2));
-    setBmi(roundNumber(NewBmi));
 }
 
 ProfileData::~ProfileData() = default;
--- src/ProfileData.h
+++ src/ProfileData.h
@@ -9,54 +9,20 @@ private:
     string Username;
     string Password;
     string Email;
-    string Firstname;
-    string Lastname;
-    string Gender;
-    signed int Age{};
-    double Height;
-    double Weight;
-    double Bmi;
 
 public:
     void setUsername(string username);
     void setPassword(string password);
     void setEmail(string email);
-    void setFirstname(string firstname);
-    void setLastname(string lastname);
-    void setGender(string gender);
-    void setAge(int age);
-    void setHeight(double height);
-    void setWeight(double weight);
-    void setBmi(double bmi);
 
     static bool validateUniqueUsername(const string &Username);
     static bool validateUsername(const string &Username);
     static bool validateUniqueEmail(const string &Email);
     static bool validateEmail(const string &Email);
     static bool validatePassword(const string &Password);
-    static bool validateFirstname(const string &FirstName);
-    static bool validateLastname(const string &LastName);
-    static bool validateGender(const string &Gender);
-    static bool validateAge(const int &Age);
-    static bool validateHeight(const double &Height);
-    static bool validateWeight(const double &Weight);
-
-//    string getUsername();
-//    string getPassword();
-//    string getEmail();
-//    string getFirstname();
-//    string getLastname();
-//    string getGender();
-
-//    [[nodiscard]] signed int getAge() const;
-//    double getHeight();
-//    double getWeight();
-    double getBmi() const;
 
     bool loginUser();
     void signupUser();
-    void calcBmi();
-    static double roundNumber(double number);
     ~ProfileData();
 };
 
